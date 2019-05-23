7f8d151c389affe0747bc04f7bdde17b63123e62
robertoeugenio1978@gmail.com <robertoeugenio>
2019-05-23 19:26:36 -0300

    Exercicio 14

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Driver } from 'selenium-webdriver/ie';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  products =[]
+  title = 'Products0';
+
+  ngOnInit(){
+    this.products = this.getProducts();
+  }
+  getProducts(){
+    return[
+      {'id': '1', 'title': 'Screw Driver', 'price': 400, 'stock' : 11 },
+      {'id': '2', 'title': 'Nut Volt', 'price': 200, 'stock' : 11 },
+      {'id': '3', 'title': 'Resistor', 'price': 78, 'stock' : 11 },
+      {'id': '4', 'title': 'Tractor', 'price': 20000, 'stock' : 11 },
+      {'id': '5', 'title': 'Roller', 'price': 62, 'stock' : 11 },
+    ];
+  }
 }
